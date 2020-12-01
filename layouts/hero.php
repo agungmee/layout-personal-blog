7f625493da012139e33fb2898c11295a408f31ff
agungmee <agunglab@gmail.com>
2020-12-02 06:34:37 +0700

    first commit for blog project php mysqli

---
+++ layouts/hero.php
@@ -0,0 +1,10 @@
+<div class="container-fluid mt-2">
+<div class="jumbotron">
+  <h1 class="display-3">Selamat Datang di Blog</h1>
+  <hr class="my-4">
+  <p>Kalo mau liat-liat kategori boleh kesini</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Lihat Kategori</a>
+  </p>
+</div>
+</div>
---
+++ layouts/navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Blogs</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item mr-2 mb-2">
+        <a class="btn btn-outline-primary" href="#">Kategori</a>
+      </li>
+      <li class="nav-item mr-2 mb-2">
+        <a class="btn btn-outline-primary" href="#">Source Code</a>
+      </li>
+      <li class="nav-item mr-2 mb-2">
+        <a class="btn btn-outline-primary" href="#">About</a>
+      </li>
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+      </li> -->
+    </ul>
+    <!-- <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form> -->
+  </div>
+</nav>
